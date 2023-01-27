3fa98c5ef28afe1dbab913a9e1bef2c48ee1084f
Aviral Vishnoi <aviralaws@gmail.com>
2023-01-27 21:34:03 +0100

    Initial commit

---
+++ tests/unit/test_postnl_event_broker_poc_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from postnl_event_broker_poc.postnl_event_broker_poc_stack import PostnlEventBrokerPocStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in postnl_event_broker_poc/postnl_event_broker_poc_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = PostnlEventBrokerPocStack(app, "postnl-event-broker-poc")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
