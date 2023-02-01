06b56c953005f1d96c6f32113a9109f6938057f0
Aviral Vishnoi <aviralaws@gmail.com>
2023-02-01 01:25:37 +0100

    Modified lambda function to send response back to api gateway and removed zip for lambda from repo

--- src/event_publisher/lambda_function.py
+++ src/event_publisher/lambda_function.py
@@ -2,28 +2,28 @@ import json
 import boto3
 import datetime
 
-client = boto3.client('events')
+client = boto3.client("events")
+
 
 def lambda_handler(event, context):
     # Replace event bus name with your event bus arn
-    
+
     response = client.put_events(
         Entries=[
             {
-            'Time': datetime.datetime.now(),
-            'Source': 'Lambda Publish',
-            'Resources': [
-             ],
-            'DetailType': 'EB Demo',
-            'Detail': json.dumps(event),
-            'EventBusName': 'arn:aws:events:eu-central-1:320722179933:event-bus/default',
-            'TraceHeader': 'testdemo'
-             },
-                ]
-             )
-        
-    return  {
-            "statusCode": 201,
-            "headers": {"Content-Type": "application/json"},
-            "body": json.loads(response)
-        }
+                "Time": datetime.datetime.now(),
+                "Source": "Lambda Publish",
+                "Resources": [],
+                "DetailType": "EB Demo",
+                "Detail": json.dumps(event),
+                "EventBusName": "arn:aws:events:eu-central-1:320722179933:event-bus/default",
+                "TraceHeader": "testdemo",
+            },
+        ]
+    )
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(response),
+    }
--- src/producer_infra_deployment/aws_utils/cloudformation.py
+++ src/producer_infra_deployment/aws_utils/cloudformation.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 
 cft_client = boto3.client("cloudformation")
 
@@ -9,19 +10,62 @@ class CFT:
         self.stack_parameters = stack_parameters
 
     def get_stack_status(self):
-        pass
+        try:
+            response = cft_client.describe_stacks(StackName = self.stack_name)
+            stack_status = response.get("Stack")[0]["StackStatus"]
+        except ClientError:
+            return False
+        return stack_status
 
     def create_stack(self):
-        pass
+        try:
+            response = cft_client.create_stack(
+                StackName=self.stack_name,
+                #TODO: Read template from s3 bucket
+                TemplateURL="s3://",
+                Parameters = self.stack_parameters,
+                Capabilities = "CAPABILITY_NAMED_IAM"
+            )
+        except ClientError as e:
+            return str(e)
+        return response
 
     def update_stack(self):
-        pass
+        try:
+            response = cft_client.update_stack(
+                StackName=self.stack_name,
+                #TODO: Read template from s3 bucket
+                TemplateURL="s3://",
+                Parameters = self.stack_parameters,
+                Capabilities = "CAPABILITY_NAMED_IAM"
+            )
+        except ClientError as e:
+            return str(e)
+        return response
     
     def check_stack_exists(self):
-        pass
-    
+        try:
+            cft_client.describe_stacks(StackName = self.stack_name)
+        except ClientError:
+            return False
+        return True
+
     def stack_requester(self):
-        pass
+        stack_exists = self.check_stack_exists()
+        if stack_exists:
+            stack_status = self.get_stack_status()
+            if stack_status in ("CREATE_COMPLETE", "UPDATE_COMPLETE"):
+                self.update_stack()
+            elif stack_status == "CREATE_IN_PROGRESS":
+                print("Stack Creation in progress")
+            else:
+                print("For now I am not handling other stack status")
+        else:
+            create_stack_response = self.create_stack()
+            if isinstance(create_stack_response, dict):
+                print("Stack creation initiated")
+            else:
+                print(f"There was an error during creation of stack {create_stack_response}")
         
         
         
