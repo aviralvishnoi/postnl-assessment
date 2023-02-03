7eb0944e06afc64c364a7de61b4252d872c64918
Aviral Vishnoi <aviralaws@gmail.com>
2023-02-04 00:32:49 +0100

    Update construct for state machine

--- postnl_event_broker_poc/custom_constructs/step_function.py
+++ postnl_event_broker_poc/custom_constructs/step_function.py
@@ -13,11 +13,16 @@ class StepFunction(Construct):
         super().__init__(scope, id)
         StepFunction.__eb_stepfunction = self.create_state_machine(props)
 
+    @property
+    def get_state_machine_arn(self):
+        return StepFunction.__eb_stepfunction.state_machine_arn
+
     def create_state_machine(self, props):
         start_state = sfn.Pass(self, "StartState")
-        sfn.StateMachine(
+        eb_state_machine = sfn.StateMachine(
             self, 
             id="EBStateMachine",
             state_machine_name=props.step_function_name,
             definition=start_state
         )
+        return eb_state_machine
